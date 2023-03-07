c846b72c7f9489e854f7187239d0c93b3f0485a8
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-03-07 21:05:15 +0100

    Update ImageCreationRequestTests.swift
    Added testSize
    Added testFormat

--- Tests/OpenAITests/ImagesTests/ImagesCreationTests/ImageCreationRequestTests.swift
+++ Tests/OpenAITests/ImagesTests/ImagesCreationTests/ImageCreationRequestTests.swift
@@ -37,4 +37,34 @@ final class ImageCreationRequestTests: XCTestCase {
         XCTAssertThrowsError(try ImageCreationRequest(description: "A green dog", quantity: 0))
     }
     
+    func testSize() throws {
+        var request = try ImageCreationRequest(description: "A green dog", size: .small)
+        var data = try encoder.encode(request)
+        var dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        var size = dict["size"] as! String
+        XCTAssertEqual(size, "256x256")
+        request = try ImageCreationRequest(description: "A green dog", size: .medium)
+        data = try encoder.encode(request)
+        dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        size = dict["size"] as! String
+        XCTAssertEqual(size, "512x512")
+        request = try ImageCreationRequest(description: "A green dog", size: .big)
+        data = try encoder.encode(request)
+        dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        size = dict["size"] as! String
+        XCTAssertEqual(size, "1024x1024")
+    }
+    
+    func testFormat() throws {
+        var request = try ImageCreationRequest(description: "A green dog", format: .url)
+        var data = try encoder.encode(request)
+        var dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        var format = dict["response_format"] as! String
+        XCTAssertEqual(format, "url")
+        request = try ImageCreationRequest(description: "A green dog", format: .b64)
+        data = try encoder.encode(request)
+        dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        format = dict["response_format"] as! String
+        XCTAssertEqual(format, "b64_json")
+    }
 }
