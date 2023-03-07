e09b58e446d0752187aff9d28b615e4dd555066b
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-03-07 03:01:30 +0100

    Update ImageCreationRequestTests.swift
    Added testDescription
    Added testLongDescriptionError
    Added testQuantity
    Added testInvalidQuantityError

--- Tests/OpenAITests/ImagesTests/ImagesCreationTests/ImageCreationRequestTests.swift
+++ Tests/OpenAITests/ImagesTests/ImagesCreationTests/ImageCreationRequestTests.swift
@@ -12,7 +12,29 @@ final class ImageCreationRequestTests: XCTestCase {
     let encoder = JSONEncoder()
     
     func testDescription() throws {
-        var request = try ImageCreationRequest(description: "A green dog")
-        var data = try encoder.encode(request)
+        let request = try ImageCreationRequest(description: "A green dog")
+        let data = try encoder.encode(request)
+        let dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        let prompt = dict["prompt"] as! String
+        XCTAssertEqual(prompt, "A green dog")
     }
+    
+    func testLongDescriptionError() throws {
+        let description = Array(repeating: "A green dog", count: 1000).joined()
+        XCTAssertThrowsError(try ImageCreationRequest(description: description))
+    }
+    
+    func testQuantity() throws {
+        let request = try ImageCreationRequest(description: "A green dog", quantity: 3)
+        let data = try encoder.encode(request)
+        let dict = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        let quantity = dict["n"] as! Int
+        XCTAssertEqual(quantity, 3)
+    }
+    
+    func testInvalidQuantityError() throws {
+        XCTAssertThrowsError(try ImageCreationRequest(description: "A green dog", quantity: 11))
+        XCTAssertThrowsError(try ImageCreationRequest(description: "A green dog", quantity: 0))
+    }
+    
 }
