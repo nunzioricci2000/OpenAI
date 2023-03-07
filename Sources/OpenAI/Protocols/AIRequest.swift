f895f73b6c6b7bcb13ad0cb29a6d44c71cfc6a26
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-03-07 02:25:12 +0100

    Update AIRequest.swift
    Removed unnecessary properties

--- Sources/OpenAI/Protocols/AIRequest.swift
+++ Sources/OpenAI/Protocols/AIRequest.swift
@@ -9,8 +9,6 @@ import Foundation
 
 protocol AIRequest: Encodable {
     associatedtype Response: AIResponse
-    static var path: String { get }
-    static var method: String { get }
     
     func urlRequest(for url: URL) throws -> URLRequest
 }
